d60858af8aa8a878f2b069520b77624cae4f5cd6
Ryan Hatfield <ryan@ryan.bio>
2023-03-11 19:18:52 +0000

    Add launch config and build task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Xml.Linq;
+using System.IO;
+using System.Diagnostics;
+
+var xml =
+    new XElement("Something",
+        new XAttribute("Att", "s"),
+        new XAttribute("Att2", "gg"),
+        new XElement("AnotherSomething"),
+        new XElement("AnotherSomething"));
+
+File.WriteAllText("something.xml", xml.ToString());
+var fileText = File.ReadAllText("something.xml");
+
+if (Debugger.IsAttached)
+{
+    // Run the debugger to hit this breakpoint, 
+    // or click the line number to add a red dot.
+    Console.WriteLine("Hit breakpoint");
+    Debugger.Break();
+}
+
+Console.WriteLine(fileText);
